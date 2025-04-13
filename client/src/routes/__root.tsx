6dbfe090ab0d122e05708531702dae8a7762400a
Catalin Pit <me+github@catalins.tech>
2025-04-13 10:03:15 +0300

    feat(client): enhance routing with session-based authentication and user greeting

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,23 +1,36 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { signOut, useSession } from "../lib/auth-client";
 
 export const Route = createRootRoute({
-  component: () => (
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const { data: session } = useSession();
+
+  return (
     <>
       <div className="p-2 flex">
         <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>{" "}
-        <Link to="/login" className="[&.active]:font-bold">
-          Login
-        </Link>{" "}
-        <Link to="/register" className="[&.active]:font-bold">
-          Register
-        </Link>
+        {session ? (
+          <button onClick={() => signOut()}>Sign Out</button>
+        ) : (
+          <>
+            <Link to="/login" className="[&.active]:font-bold">
+              Login
+            </Link>{" "}
+            <Link to="/register" className="[&.active]:font-bold">
+              Register
+            </Link>
+          </>
+        )}
       </div>
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </>
-  ),
-});
+  );
+}
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,13 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
-
+import { useSession } from "../lib/auth-client";
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  const { data: session } = useSession();
+
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      {session && <div>Hello {session.user.name}</div>}
     </div>
   );
 }
